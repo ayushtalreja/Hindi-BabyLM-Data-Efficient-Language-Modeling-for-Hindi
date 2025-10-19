338310e32f0df67134bba5b3e5ccf738fca2d275
ayushkumar <ge95xod@mytum.de>
2025-10-19 14:25:18 +0200

    -

--- slurm_scripts/run_evaluation.sh
+++ slurm_scripts/run_evaluation.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/eval_%j.out
 #SBATCH --error=logs/eval_%j.err
 #SBATCH --time=14:00:00
-#SBATCH --partition=mcml-hgx-a100-80x4-mig   
+#SBATCH --partition=lrz-hgx-h100-94x4      
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
